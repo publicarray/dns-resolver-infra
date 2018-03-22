478efa9275da95487de71cd50bc3f42472ba60f1
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-03-22 22:08:46 +1000

    dnscrypt-wrapper

---
+++ dnscrypt-wrapper/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+UNBOUND_SERVICE_HOST=${UNBOUND_SERVICE_HOST-"9.9.9.9"}
+UNBOUND_SERVICE_PORT=${UNBOUND_SERVICE_PORT-"53"}
+#-r "${UNBOUND_SERVICE_HOST}:${UNBOUND_SERVICE_PORT}"--listen-address 0.0.0.0:443
+
+if [ $# -eq 0 ]; then
+    # chroot /opt/dnscrypt /usr/local/sbin/dnscrypt-wrapper --user=_dnscrypt-wrapper -h
+    # chroot /etc/dnscrypt-wrapper/run/ /usr/local/sbin/dnscrypt-wrapper --user=_dnscrypt-wrapper
+    exec /usr/local/sbin/dnscrypt-wrapper --user=_dnscrypt-wrapper -h
+fi
+
+[ "$1" = '--' ] && shift
+
+exec "$@"
+# exec /usr/local/sbin/dnscrypt-wrapper --user="$DNSCRYPT_USER" "$@"
+
+# empty cmd
+# -> help
+
+# cmd of -d dns.seby.io
+# -> run
+
+# cmd of -d dns.seby.io --init
