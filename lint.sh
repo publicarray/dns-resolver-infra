7d659ea43aae96a690f3fb9d269fdba8eeae661c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-06-11 21:34:35 +1000

    lint

--- lint.sh
+++ lint.sh
@@ -3,3 +3,9 @@
 # yarn
 find . -not -path "./node_modules/*" -type f -name Dockerfile | xargs -L1 node_modules/.bin/dockerlint
 find . -not -path "./node_modules/*" -type f -name Dockerfile | xargs -L1 node_modules/.bin/dockerfile_lint -f
+
+if command -v hadolint >/dev/null; then
+    find . -not -path "./node_modules/*" -type f -name Dockerfile | xargs -L1 hadolint
+else
+    echo "For more linting install hadolint 'brew install hadolint'"
+fi
